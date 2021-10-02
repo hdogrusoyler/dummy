664e8e48328d8acb54162fa2e44c9fac6361ebc9
HUS <HUS@LAPTOP>
2021-10-02 16:23:11 +0300

    .

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -5,32 +5,30 @@ import { ServiceService } from '../service.service';
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.css']
+  styleUrls: ['./comment.component.css'],
 })
 export class CommentComponent implements OnInit {
-
   constructor(
     private service: ServiceService,
     private activatedRoute: ActivatedRoute
-    ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.data.subscribe(data => {
-      console.log(data)
+    this.activatedRoute.data.subscribe((data) => {
+      console.log(data);
     });
-    this.activatedRoute.params.subscribe(params => {
+    this.activatedRoute.params.subscribe((params) => {
       this.getComments(params['postId']);
     });
   }
 
-  comments: Array<Comment>=[];
+  comments: Array<Comment> = [];
 
-  getComments(id:number){
-    this.service.getComment(id).subscribe(result => {
+  getComments(id: number) {
+    this.service.getComment(id).subscribe((result) => {
       this.comments = result;
-    })
+    });
   }
 
-  displayedColumns: string[] = ['id','name', 'body', 'email', 'postId'];
-
+  displayedColumns: string[] = ['id', 'name', 'body', 'email', 'postId'];
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -7,35 +7,37 @@ import { ServiceService } from '../service.service';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
-
-  constructor(private actRoute: ActivatedRoute,private service: ServiceService) { }
+  constructor(
+    private actRoute: ActivatedRoute,
+    private service: ServiceService
+  ) {}
 
   ngOnInit(): void {
-    this.actRoute.data.subscribe(data => {
-      console.log(data)
-    })
+    this.actRoute.data.subscribe((data) => {
+      console.log(data);
+    });
     this.getPosts();
   }
-  
-  posts: Array<Post>=[];
-  postlist: Array<Post>=[];
 
-  getPosts(){
-    this.service.getPost().subscribe(result => {
+  posts: Array<Post> = [];
+  postlist: Array<Post> = [];
+
+  getPosts() {
+    this.service.getPost().subscribe((result) => {
       this.posts = result;
       this.getPageEvent(this.pageEvent);
-    })
+    });
   }
-  displayedColumns: string[] = ['actions','id', 'title', 'body', 'userId'];
+  displayedColumns: string[] = ['actions', 'id', 'title', 'body', 'userId'];
   pageEvent: PageEvent = {
     pageIndex: 0,
     pageSize: 5,
-    length: 0 
+    length: 0,
   };
-  length:number|undefined;
+  length: number | undefined;
   pageSize = 10;
   pageSizeOptions: number[] = [5, 10, 25, 100];
   pageIndex = 0;
@@ -43,12 +45,14 @@ export class PostComponent implements OnInit {
 
   setPageSizeOptions(setPageSizeOptionsInput: string) {
     if (setPageSizeOptionsInput) {
-      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+      this.pageSizeOptions = setPageSizeOptionsInput
+        .split(',')
+        .map((str) => +str);
     }
   }
 
-  getPageEvent(event:any) {
-    const firstIndex = (event.pageIndex * event.pageSize);
+  getPageEvent(event: any) {
+    const firstIndex = event.pageIndex * event.pageSize;
     const lastIndex = firstIndex + event.pageSize;
     this.postlist = this.posts.slice(firstIndex, lastIndex);
     this.pageIndex = event.pageIndex;
